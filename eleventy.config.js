3618717529ecb66a5f59f7b4e42a426211e69425
Gaya Kessler <gaya@theclevernode.com>
2025-09-24 13:38:39 +0200

    add 11ty image plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
+import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
+
 export default function(eleventyConfig) {
   eleventyConfig.ignores.add("includes/**");
+
   eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 };
